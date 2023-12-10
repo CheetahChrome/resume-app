4b159f14d984f2883214cc967502b1e96ff7afcf
Hyperion <William@Wegerson.com>
2023-12-09 21:47:53 -0700

    Working Service - Yay

--- src/app/Services/get-jobs.service.ts
+++ src/app/Services/get-jobs.service.ts
@@ -1,9 +1,15 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Job } from '../Interfaces/Company';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GetJobsService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  public getAllJobs$(){
+    return this.http.get<Job[]>('/assets/mocks/Companies.json')
+  }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { Job } from './Interfaces/Company';
+import { GetJobsService } from './Services/get-jobs.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+
+export class AppComponent implements OnInit {
   title = 'resume-app';
+
+  public jobs$: Observable<Job[] | undefined> = of(undefined);
+
+  constructor(private api:GetJobsService) { }
+
+
+  public ngOnInit(): void {
+    this.jobs$ = this.api.getAllJobs$();
+  }
+
 }
