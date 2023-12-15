fed030309dd2355391287ea08fdd9a3339934b54
Hyperion <William@Wegerson.com>
2023-12-15 15:47:55 -0700

    Component JobCarousel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { MatSidenavModule } from '@angular/material/sidenav'
-import { MatTabsModule } from '@angular/material/tabs';
 import { MatCardModule } from '@angular/material/card';
-import { MatIconModule } from '@angular/material/icon';
 import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav'
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatTabsModule } from '@angular/material/tabs';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 import { BoldWordsPipe } from './Pipes/bold-words.pipe';
 import { FilterTechPipe } from './Pipes/filter-tech.pipe';
 import { FooterComponent } from './Components/footer/footer.component';
+import { JobCarouselComponent } from './Components/job-carousel/job-carousel.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BoldWordsPipe,
     FilterTechPipe,
-    FooterComponent
+    FooterComponent,
+    JobCarouselComponent
   ],
   imports: [
     AppRoutingModule,
